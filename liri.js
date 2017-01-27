2cc568b9590f0c37a1da2b52a0523977d84a467a
vincent_visconti <vinnyv88@gmail.com>
2017-01-27 14:08:00 -0500

    implemented spotify search.

--- liri.js
+++ liri.js
@@ -6,6 +6,7 @@ var twit_token_secret = k.twitterKeys.access_token_secret;
 
 var Twitter = require('twitter');
 var moment = require('moment');
+var spotify = require('spotify');
 
 var command = process.argv[2];
 
@@ -34,10 +35,38 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  console.log(error);
   } 
 });
-
 	
-}
+} // end myTweets function
 
+function spotifyThisSong() {
+
+// var params = {
+// 	screen_name: 'VinnyV88',
+// 	count: 20
+// };
+
+spotify.search({ type: 'track', query: 'ace of base the sign' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+	var artistsCount = data.tracks.items[0].album.artists.length;
+	var artists = "Artist(s): " + data.tracks.items[0].album.artists[0].name;
+	if (artistsCount > 1) {
+		for (var i = 1; i < artistsCount; i++) {
+			artists = artists + ", " + data.tracks.items[0].album.artists[i].name
+		}
+	}
+
+	var songTitle = "Song Title: " + data.tracks.items[0].name;
+	var previewURL = "Preview URL: " + data.tracks.items[0].preview_url;
+	var albumTitle = "Album Title: " + data.tracks.items[0].album.name;
+
+	console.log(artists + "\n" + songTitle + "\n" + previewURL + "\n" + albumTitle + "\n" );
+    // Do something with 'data' 
+});
+	
+} // end spotifyThisSong function
 
 
 
