2e9680c8920083a5e9a63c06aed1ff96e28cf2e6
vincent_visconti <vinnyv88@gmail.com>
2017-01-27 09:42:40 -0500

    got twitter working. installed moment.js to format twitter date.

--- liri.js
+++ liri.js
@@ -1,13 +1,39 @@
 var k = require("./keys.js");
-var twit_key = k.twitter_keys.consumer_key;
-var twit_secret = k.twitter_keys.consumer_secret;
-var twit_token_key = k.twitter_keys.access_token_key;
-var twit_token_secret = k.twitter_keys.access_token_secret;
+var twit_key = k.twitterKeys.consumer_key;
+var twit_secret = k.twitterKeys.consumer_secret;
+var twit_token_key = k.twitterKeys.access_token_key;
+var twit_token_secret = k.twitterKeys.access_token_secret;
+
+var Twitter = require('twitter');
+var moment = require('moment');
 
 var command = process.argv[2];
 
 function myTweets() {
 
+var client = new Twitter({
+  consumer_key: twit_key,
+  consumer_secret: twit_secret,
+  access_token_key: twit_token_key,
+  access_token_secret: twit_token_secret
+});
+
+var params = {
+	screen_name: 'VinnyV88',
+	count: 20
+};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if(!error) {
+	for (var i=0; i < tweets.length; i++) {
+	  console.log(tweets[i].text);
+	  console.log(moment(tweets[i].created_at).format("LLLL"));
+	  console.log("");	
+	}  
+  } else {
+	  console.log(error);
+  } 
+});
 
 	
 }
