8ec41e29dd6ec9b8ea65b8f43e2531d0bcb81379
vincent_visconti <vinnyv88@gmail.com>
2017-01-29 01:55:22 -0500

    implemented do-what-it-says functionality.

--- liri.js
+++ liri.js
@@ -8,35 +8,53 @@ var Twitter = require('twitter');
 var moment = require('moment');
 var spotify = require('spotify');
 var request = require('request');
+var fs = require('fs');
 
-var command = process.argv[2];
-var qryParm = "";
+function execCommand(cmd) {
+
+	switch (cmd) {
+		case "my-tweets":
+			//do twitter stuff
+			myTweets();
+			break;
+		case "spotify-this-song":
+			//do spotify stuff
+			spotifyThisSong();
+			break;
+		case "movie-this":
+			//do omdb stuff
+			movieThis();
+			break;
+		case "do-what-it-says":
+			//do stuff
+			doWhatItSays();
+			break;
+		default:
+			console.log("Invalid command! Please try again.")
+	}
+
+}
 
 function getArguments() {
+	var args = "";
 	if (!(process.argv[3] == null)) {
 	var i = 3;
-	qryParm = process.argv[i];
+	args = process.argv[i];
 	i++;
 	while (!(process.argv[i] == null)) {
-		qryParm = qryParm + "+" + process.argv[i];
+		args = args + "+" + process.argv[i];
 		i++; 
 		} 
 	}
+	return args;
+}
 
-	qryParm = qryParm.replace(" ", "+");
-	qryParm = qryParm.replace(".", "");
-	qryParm = qryParm.replace("-", "");
-	qryParm = qryParm.replace("(", "");
-	qryParm = qryParm.replace(")", "");
-	qryParm = qryParm.replace("_", "");
-	qryParm = qryParm.replace(".", "");
-	qryParm = qryParm.replace("!", "");
-	qryParm = qryParm.replace("@", "");
-	qryParm = qryParm.replace("#", "");
-	qryParm = qryParm.replace("$", "");
-	qryParm = qryParm.replace("%", "");
-	qryParm = qryParm.replace("^", "");
-	qryParm = qryParm.replace("&", ""); 
+function formatParameters(parms) {
+	//remove special characters that would break api search and replace space with '+' 
+	parms = parms.replace(/[^a-zA-Z + 0-9]+/g,'');
+	parms = parms.replace(/ /g, "+");
+
+	return parms;
 }
 
 function myTweets() {
@@ -57,7 +75,10 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if(!error) {
 	for (var i=0; i < tweets.length; i++) {
 	  console.log(tweets[i].text);
-	  console.log(moment(tweets[i].created_at).format("LLLL"));
+	//   console.log(tweets[i].created_at);
+	  var tweetDate = tweets[i].created_at;
+	  var formattedDate = moment(tweetDate).format('LLLL', false);
+	  console.log(formattedDate);
 	  console.log("");	
 	}  
   } else {
@@ -69,9 +90,7 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
 function spotifyThisSong() {
 
-qryParm = "ace of base the sign";
-
-getArguments();
+if (qryParm === "") qryParm = "ace+of+base+the+sign";
 
 spotify.search({ type: 'track', query: qryParm }, function(err, data) {
     if ( err ) {
@@ -84,7 +103,7 @@ spotify.search({ type: 'track', query: qryParm }, function(err, data) {
 		var artists = "Artist(s): " + data.tracks.items[0].album.artists[0].name;
 		if (artistsCount > 1) {
 			for (var i = 1; i < artistsCount; i++) {
-				artists = artists + ", " + data.tracks.items[0].album.artists[i].name
+				artists = artists + "/g, " + data.tracks.items[0].album.artists[i].name
 			}
 		}
 
@@ -103,54 +122,52 @@ spotify.search({ type: 'track', query: qryParm }, function(err, data) {
 
 function movieThis() {
 
-qryParm = "mr. nobody";
-
-getArguments();
+if (qryParm === "") qryParm = "mr+nobody";
 
 requestURL = "http://www.omdbapi.com/?t=" + qryParm + "&tomatoes=true";
 
 request(requestURL, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-	var movieTitle = "Movie Title: " + JSON.parse(body).Title;
-	var releaseYr = "Release Year: " + JSON.parse(body).Year;
-	var imdbRating = "IMDB Rating: " + JSON.parse(body).imdbRating;
-	var cntry = "Country(ies) Produced in: " + JSON.parse(body).Country;
-	var lang = "Movie Language(s): " + JSON.parse(body).Language;
-	var plot = "Movie Plot: " + JSON.parse(body).Plot;
-	var actors = "Actors: " + JSON.parse(body).Actors;
-	var rtRating = "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating;
-	var rtURL = "Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL;
+	  if (!(JSON.parse(body).Response == "False")) {
+		var movieTitle = "Movie Title: " + JSON.parse(body).Title;
+		var releaseYr = "Release Year: " + JSON.parse(body).Year;
+		var imdbRating = "IMDB Rating: " + JSON.parse(body).imdbRating;
+		var cntry = "Country(ies) Produced in: " + JSON.parse(body).Country;
+		var lang = "Movie Language(s): " + JSON.parse(body).Language;
+		var plot = "Movie Plot: " + JSON.parse(body).Plot;
+		var actors = "Actors: " + JSON.parse(body).Actors;
+		var rtRating = "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating;
+		var rtURL = "Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL;
 
-	console.log("\n" + movieTitle + "\n" + releaseYr + "\n" + imdbRating + 
-	"\n" + cntry + "\n" + lang + "\n" + plot + "\n" + actors + "\n" + rtRating + "\n" + rtURL + "\n" );
-	} else {
-	console.log("\n" + "No movie found! Please try again.");
-	}
+		console.log("\n" + movieTitle + "\n" + releaseYr + "\n" + imdbRating + 
+		"\n" + cntry + "\n" + lang + "\n" + plot + "\n" + actors + "\n" + rtRating + "\n" + rtURL + "\n" );
+		} else {
+		console.log("\n" + "No movie found! Please try again.");
+		}
+  }
 });
 
 } // end movieThis function
 
+function doWhatItSays() {
 
+fs.readFile('random.txt', 'utf8', function(error, data) {
+	var dataArr = data.split(',');
+	var command = dataArr[0];
+	var parameters = dataArr[1];
 
+	if (!(parameters == null)) qryParm = formatParameters(parameters);
+	execCommand(command);
 
+});
 
-//program starts here
-
-switch (command) {
-	case "my-tweets":
-		//do twitter stuff
-		myTweets();
-		break;
-	case "spotify-this-song":
-		//do spotify stuff
-		spotifyThisSong();
-		break;
-	case "movie-this":
-		//do omdb stuff
-		movieThis();
-		break;
-	case "do-what-it-says":
-		//do stuff
-		doWhatItSays();
 }
 
+//program starts here
+var arguments = "";
+var qryParm = "";
+
+arguments = getArguments();
+qryParm = formatParameters(arguments);
+
+execCommand(process.argv[2]);
