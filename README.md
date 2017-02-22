35b08ec09efad172c76b491e4f80f62b3534783a
vincent_visconti <vinnyv88@gmail.com>
2017-02-21 21:34:32 -0500

    updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# liri-node-app
+# LIRI Node App
+
+## Synopsis:
+LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will is a command line node app that takes in parameters and gives you back data.
+
+#Commands Available
+- node liri.js my-tweets
+  This will show your last 20 tweets and when they were created at in your terminal/bash window.
+- node liri.js spotify-this-song '<song name here>'
+  This will show Artist, Title, Album, and a link to spotify's song preview
+- node liri.js movie-this '<movie name here>'
+  This will show various information related to the movie name entered
+- node liri.js do-what-it-says
+  Will run the command located in the random.txt file
+
+<a href="https://media.giphy.com/media/d3LFY4FZ3tIhA9DW/source.gif" target="_blank"><img src="http://i.giphy.com/d3LFY4FZ3tIhA9DW.gif" ></a>
+
+## Technologies used
+- JavaScript
+- Node.js
+- Spotify API
+- Twitter API
+- OMDB API
+- fs (file system)
+
+## Built With
+* Visual Studio Code 
+
+## Author
+* [Vincent Visconti](https://github.com/VinnyV88)
+  
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- liri.js
+++ liri.js
@@ -41,20 +41,20 @@ function execCommand(cmd) {
 function getArguments() {
 	var args = "";
 	if (!(process.argv[3] == null)) {
-	var i = 3;
-	args = process.argv[i];
-	i++;
-	while (!(process.argv[i] == null)) {
-		args = args + "+" + process.argv[i];
-		i++; 
-		} 
+		var i = 3;
+		args = process.argv[i];
+		i++;
+		while (!(process.argv[i] == null)) {
+			args = args + "+" + process.argv[i];
+			i++;
+		}
 	}
 	return args;
 }
 
 function formatParameters(parms) {
 	//remove special characters that would break api search and replace space with '+' 
-	parms = parms.replace(/[^a-zA-Z + 0-9]+/g,'');
+	parms = parms.replace(/[^a-zA-Z + 0-9]+/g, '');
 	parms = parms.replace(/ /g, "+");
 
 	return parms;
@@ -68,7 +68,7 @@ function consoleAndFileLog(txt) {
 function fileLog(txt) {
 	var txtFile = "log.txt";
 
-	fs.appendFile(txtFile, txt + os.EOL, function(err) {
+	fs.appendFile(txtFile, txt + os.EOL, function (err) {
 		if (err) {
 			console.log("error: " + err);
 			return;
@@ -78,106 +78,109 @@ function fileLog(txt) {
 
 function myTweets() {
 
-var client = new Twitter({
-  consumer_key: twit_key,
-  consumer_secret: twit_secret,
-  access_token_key: twit_token_key,
-  access_token_secret: twit_token_secret
-});
+	var client = new Twitter({
+		consumer_key: twit_key,
+		consumer_secret: twit_secret,
+		access_token_key: twit_token_key,
+		access_token_secret: twit_token_secret
+	});
 
-var params = {
-	screen_name: 'VinnyV88',
-	count: 20
-};
-var writeThis = "";
+	var params = {
+		screen_name: 'VinnyV88',
+		count: 20
+	};
+	var writeThis = "";
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if(!error) {
-	for (var i=0; i < tweets.length; i++) {
-	  var tweetDate = Date.parse(tweets[i].created_at.replace(/( \+)/, ' UTC$1'));
-	  var formattedDate = moment(tweetDate).format('LLLL');
-	  writeThis = tweets[i].text + os.EOL + formattedDate + os.EOL;	
-	  consoleAndFileLog(writeThis);
-	}  
-  } else {
-	  consoleAndFileLog(error);
-  } 
-});
-	
+	client.get('statuses/user_timeline', params, function (error, tweets, response) {
+		if (!error) {
+			for (var i = 0; i < tweets.length; i++) {
+				var tweetDate = Date.parse(tweets[i].created_at.replace(/( \+)/, ' UTC$1'));
+				var formattedDate = moment(tweetDate).format('LLLL');
+				writeThis = tweets[i].text + os.EOL + formattedDate + os.EOL;
+				consoleAndFileLog(writeThis);
+			}
+		} else {
+			consoleAndFileLog(error);
+		}
+	});
+
 } // end myTweets function
 
 function spotifyThisSong() {
 
-if (qryParm === "") qryParm = "ace+of+base+the+sign";
+	if (qryParm === "") qryParm = "ace+of+base+the+sign";
 
-spotify.search({ type: 'track', query: qryParm }, function(err, data) {
-    if ( err ) {
-        consoleAndFileLog('Error occurred: ' + err);
-        return;
-    }
-
-	if (data.tracks.total > 0) { 
-		var artistsCount = data.tracks.items[0].album.artists.length;
-		var artists = "Artist(s): " + data.tracks.items[0].album.artists[0].name;
-		if (artistsCount > 1) {
-			for (var i = 1; i < artistsCount; i++) {
-				artists = artists + "/g, " + data.tracks.items[0].album.artists[i].name
-			}
+	spotify.search({
+		type: 'track',
+		query: qryParm
+	}, function (err, data) {
+		if (err) {
+			consoleAndFileLog('Error occurred: ' + err);
+			return;
 		}
 
-		var songTitle = "Song Title: " + data.tracks.items[0].name;
-		var previewURL = "Preview URL: " + data.tracks.items[0].preview_url;
-		var albumTitle = "Album Title: " + data.tracks.items[0].album.name;
+		if (data.tracks.total > 0) {
+			var artistsCount = data.tracks.items[0].album.artists.length;
+			var artists = "Artist(s): " + data.tracks.items[0].album.artists[0].name;
+			if (artistsCount > 1) {
+				for (var i = 1; i < artistsCount; i++) {
+					artists = artists + "/g, " + data.tracks.items[0].album.artists[i].name
+				}
+			}
 
-		consoleAndFileLog(os.EOL + artists + os.EOL + songTitle + os.EOL + previewURL + os.EOL + albumTitle + os.EOL );
-	} else {
-		consoleAndFileLog(os.EOL + "No tracks were found! Please try again.");
-	}
+			var songTitle = "Song Title: " + data.tracks.items[0].name;
+			var previewURL = "Preview URL: " + data.tracks.items[0].preview_url;
+			var albumTitle = "Album Title: " + data.tracks.items[0].album.name;
 
-});
-	
+			consoleAndFileLog(os.EOL + artists + os.EOL + songTitle + os.EOL + previewURL + os.EOL + albumTitle + os.EOL);
+		} else {
+			consoleAndFileLog(os.EOL + "No tracks were found! Please try again.");
+		}
+
+	});
+
 } // end spotifyThisSong function
 
 function movieThis() {
 
-if (qryParm === "") qryParm = "mr+nobody";
+	if (qryParm === "") qryParm = "mr+nobody";
 
-requestURL = "http://www.omdbapi.com/?t=" + qryParm + "&tomatoes=true";
+	requestURL = "http://www.omdbapi.com/?t=" + qryParm + "&tomatoes=true";
 
-request(requestURL, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-	  if (!(JSON.parse(body).Response == "False")) {
-		var movieTitle = "Movie Title: " + JSON.parse(body).Title;
-		var releaseYr = "Release Year: " + JSON.parse(body).Year;
-		var imdbRating = "IMDB Rating: " + JSON.parse(body).imdbRating;
-		var cntry = "Country(ies) Produced in: " + JSON.parse(body).Country;
-		var lang = "Movie Language(s): " + JSON.parse(body).Language;
-		var plot = "Movie Plot: " + JSON.parse(body).Plot;
-		var actors = "Actors: " + JSON.parse(body).Actors;
-		var rtRating = "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating;
-		var rtURL = "Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL;
+	request(requestURL, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			if (!(JSON.parse(body).Response == "False")) {
+				var movieTitle = "Movie Title: " + JSON.parse(body).Title;
+				var releaseYr = "Release Year: " + JSON.parse(body).Year;
+				var imdbRating = "IMDB Rating: " + JSON.parse(body).imdbRating;
+				var cntry = "Country(ies) Produced in: " + JSON.parse(body).Country;
+				var lang = "Movie Language(s): " + JSON.parse(body).Language;
+				var plot = "Movie Plot: " + JSON.parse(body).Plot;
+				var actors = "Actors: " + JSON.parse(body).Actors;
+				var rtRating = "Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating;
+				var rtURL = "Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL;
 
-		consoleAndFileLog(os.EOL + movieTitle + os.EOL + releaseYr + os.EOL + imdbRating + 
-		os.EOL + cntry + os.EOL + lang + os.EOL + plot + os.EOL + actors + os.EOL + rtRating + os.EOL + rtURL + os.EOL );
-		} else {
-		consoleAndFileLog(os.EOL + "No movie found! Please try again.");
+				consoleAndFileLog(os.EOL + movieTitle + os.EOL + releaseYr + os.EOL + imdbRating +
+					os.EOL + cntry + os.EOL + lang + os.EOL + plot + os.EOL + actors + os.EOL + rtRating + os.EOL + rtURL + os.EOL);
+			} else {
+				consoleAndFileLog(os.EOL + "No movie found! Please try again.");
+			}
 		}
-  }
-});
+	});
 
 } // end movieThis function
 
 function doWhatItSays() {
 
-fs.readFile('random.txt', 'utf8', function(error, data) {
-	var dataArr = data.split(',');
-	var command = dataArr[0];
-	var parameters = dataArr[1];
+	fs.readFile('random.txt', 'utf8', function (error, data) {
+		var dataArr = data.split(',');
+		var command = dataArr[0];
+		var parameters = dataArr[1];
 
-	if (!(parameters == null)) qryParm = formatParameters(parameters);
-	execCommand(command);
+		if (!(parameters == null)) qryParm = formatParameters(parameters);
+		execCommand(command);
 
-});
+	});
 
 }
 
